c56148c3022ced797085cd86958a379876d5b5b8
Alexander Tih <dgioto@gmail.com>
2024-04-02 16:09:40 +0300

    Added transition from MainScreen to fairy tale page DetailScreen

--- app/src/main/java/com/dgioto/fairytalesinukrainian/ui/MainScreen.kt
+++ app/src/main/java/com/dgioto/fairytalesinukrainian/ui/MainScreen.kt
@@ -1,8 +1,10 @@
 package com.dgioto.fairytalesinukrainian.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,18 +14,24 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -41,87 +49,139 @@ import com.dgioto.fairytalesinukrainian.ui.theme.FairyTalesInUkrainianTheme
 
 @Composable
 fun MainScreen() {
+    val navigateToDetail = remember { mutableStateOf(false) }
+    var selectedFairytale by remember { mutableStateOf<FairyTale?>(null) }
 
-    Scaffold(
-        bottomBar = {
-            androidx.compose.material3.NavigationBar {
-                val selectedItemPosition = remember {
-                    mutableIntStateOf(0)
-                }
+    if (navigateToDetail.value) {
+        DetailScreen(selectedFairytale!!, onBackClick = { navigateToDetail.value = false })
+    } else {
+        Scaffold(
+            bottomBar = {
+                androidx.compose.material3.NavigationBar {
+                    val selectedItemPosition = remember {
+                        mutableIntStateOf(0)
+                    }
 
-                val items = listOf(
-                    NavigationItem.Home,
-                    NavigationItem.Favourite,
-                    NavigationItem.Profile
-                )
-
-                items.forEachIndexed { index, item ->
-                    NavigationBarItem(
-                        selected = selectedItemPosition.intValue == index,
-                        onClick = { selectedItemPosition.intValue = index },
-                        icon = {
-                            Icon(item.icon, contentDescription = null)
-                        },
-                        label = {
-                            Text(text = stringResource(id = item.titleResId))
-                        }
+                    val items = listOf(
+                        NavigationItem.Home,
+                        NavigationItem.Favourite,
+                        NavigationItem.Profile
                     )
+
+                    items.forEachIndexed { index, item ->
+                        NavigationBarItem(
+                            selected = selectedItemPosition.intValue == index,
+                            onClick = { selectedItemPosition.intValue = index },
+                            icon = {
+                                Icon(item.icon, contentDescription = null)
+                            },
+                            label = {
+                                Text(text = stringResource(id = item.titleResId))
+                            }
+                        )
+                    }
                 }
             }
-        }
-    ) {
-        val items = listOf(
-            FairyTale(
-                image = R.drawable.koza_dereza,
-                title = "Коза дереза",
-                description = "Description 1",
-                isFavorite = false
-            ),
-            FairyTale(
-                image = R.drawable.telesik,
-                title = "Телесик",
-                description = "Description 2",
-                isFavorite = false
-            ),
-            FairyTale(
-                image = R.drawable.pan_kozkiy,
-                title = "Пан коцький",
-                description = "Description 3",
-                isFavorite = false
-            ),
-        )
+        ) {
+            val items = listOf(
+                FairyTale(
+                    image = R.drawable.koza_dereza,
+                    title = "Коза дереза",
+                    description = "Description 1",
+                    isFavorite = false
+                ),
+                FairyTale(
+                    image = R.drawable.telesik,
+                    title = "Телесик",
+                    description = "Description 2",
+                    isFavorite = false
+                ),
+                FairyTale(
+                    image = R.drawable.pan_kozkiy,
+                    title = "Пан коцький",
+                    description = "Description 3",
+                    isFavorite = false
+                ),
+            )
 
-        LazyColumn(modifier = Modifier.padding(it)) {
-            items(items) { item ->
-                FairyTaleItem(fairyTale = item)
+            LazyColumn(modifier = Modifier.padding(it)) {
+                items(items) { item ->
+                    // Обработчик нажатия на FairyTaleItem
+                    val onItemClick: () -> Unit = {
+                        navigateToDetail.value = true
+                        selectedFairytale = item
+                    }
+                    // Вызов FairyTaleItem с передачей обработчика нажатия
+                    FairyTaleItem(fairyTale = item, onItemClick = onItemClick)
+                }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FairyTaleItem(fairyTale: FairyTale) {
+fun DetailScreen(fairyTale: FairyTale, onBackClick: () -> Unit) {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column {
+            TopAppBar(
+                title = { Text(text = fairyTale.title) },
+                navigationIcon = {
+                    IconButton(onClick = onBackClick) {
+                        Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* Handle favorite toggle */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Favorite, // Adjust based on favorite state
+                            contentDescription = null,
+                            tint = Color.Red // Adjust based on favorite state
+                        )
+                    }
+                }
+            )
+
+            Image(
+                painter = painterResource(id = fairyTale.image),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+            )
+
+            Text(
+                text = fairyTale.description,
+                modifier = Modifier.padding(all = 16.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun FairyTaleItem(fairyTale: FairyTale, onItemClick: () -> Unit) {
     val isFavoriteState = remember { mutableStateOf(fairyTale.isFavorite) }
 
     Card(
         modifier = Modifier.padding(start = 8.dp, end = 8.dp, top = 4.dp, bottom = 4.dp)
     ) {
+        // Обработчик нажатия добавлен к Row
         Row(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth().clickable(onClick = onItemClick),
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            Box(modifier = Modifier
-                .size(100.dp)
-                .padding(start = 8.dp, top = 8.dp, bottom = 8.dp)
-                .clip(RoundedCornerShape(15.dp))
-            ){
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(start = 8.dp, top = 8.dp, bottom = 8.dp)
+                    .clip(RoundedCornerShape(15.dp))
+            ) {
                 Image(
                     painter = painterResource(id = fairyTale.image),
                     contentDescription = null,
                     modifier = Modifier.fillMaxSize()
                 )
             }
-
             Text(
                 text = fairyTale.title,
                 style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Bold),
@@ -137,7 +197,7 @@ fun FairyTaleItem(fairyTale: FairyTale) {
             ) {
                 Icon(
                     imageVector = if (isFavoriteState.value) Icons.Filled.Favorite
-                                    else Icons.Outlined.Favorite,
+                    else Icons.Outlined.Favorite,
                     contentDescription = null,
                     tint = if (isFavoriteState.value) Color.Red else Color.Gray
                 )
@@ -150,6 +210,7 @@ fun FairyTaleItem(fairyTale: FairyTale) {
 fun LightThemePreview() {
     FairyTalesInUkrainianTheme(darkTheme = false) { MainScreen() }
 }
+
 @Preview
 @Composable
 fun DarkThemePreview() {
